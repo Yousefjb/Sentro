83c4460b0124a80e6325f421de73c952c75c02b6
abdullahdawoud@gmail.com <abdullahdawoud@gmail.com>
2016-01-31 16:32:10 +0200

    5 test

---
+++ Sentro/CacheManager/CacheManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentro.CacheManager
+{
+    class CacheManager
+    {
+
+        //Empty Class
+    }
+}
