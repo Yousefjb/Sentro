7a3f5156e15ef898d14b251e0c0377f2b0a7bf84
Abdullah Dawoud <abdullahdawoud@gmail.com>
2016-02-13 19:43:26 +0200

    fix

--- Sentro/Program.cs
+++ Sentro/Program.cs
@@ -10,15 +10,12 @@ namespace Sentro
             Sentro.CacheManager.CacheManager obj = new CacheManager.CacheManager();
             string given_url = "";
             obj.Hier();
-            do
-            {
+            
                 //Console.Write("\nInput: ");
                 //given_url = Console.ReadLine();
                 //string _normalized = obj.normalize(given_url);
                 //string _hashed = obj.hash(_normalized);
-                
-
-            } while (given_url!="*");
+             
             
             
         }
